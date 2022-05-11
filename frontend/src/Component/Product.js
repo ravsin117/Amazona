d299c79d12ee5766f9906aeda75c0cb53dc8b585
Ravinder <ravsin117@gmail.com>
2022-05-11 14:58:13 +0530

    Ratings added to each product

--- frontend/src/Component/Product.js
+++ frontend/src/Component/Product.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Rating from './Rating';
 
 
 const Product = (props) => {
@@ -10,20 +11,25 @@ const Product = (props) => {
   return (
     
       <Card key={product.slug}>
+        
         <Link to={`/product/${product.slug}`}>
           <img className='card-img-top' src={product.image} alt={product.name} />
         </Link>
+
         <Card.Body>
+
           <Link to={`/product/${product.slug}`}>
             <Card.Title>{product.name}</Card.Title>
           </Link>
+
+          <Rating rating={product.rating} numReviews={product.numReviews}/>
           <Card.Text>
             ${product.price}
           </Card.Text>
+
           <Button> Add to cart</Button>
         </Card.Body>
-        <div className="product-info">
-        </div>
+
     </Card>
   );
 }
--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -59,7 +59,7 @@ const HomeScreen = () => {
           <Row>
             {
               products.map((product) => (
-                <Col sm={6} md={4} lr={3} className='mb-3'>
+                <Col key={product.slug} sm={6} md={4} lr={3} className='mb-3'>
                 <Product product={product}></Product>
                 </Col>
               ))
